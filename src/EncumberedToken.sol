90e60cc10e27e1d99d71bc5aa1e7295be7abacf2
steven <steven.nevins.js@gmail.com>
2024-06-05 12:35:21 -0400

    clean up

--- src/EncumberedToken.sol
+++ src/EncumberedToken.sol
@@ -9,7 +9,7 @@ interface IEncumberable {
      * @param taker The address which will have the right to release the encumbered tokens.
      * @param amount The amount of tokens to be encumbered.
      */
-    function encumber(address taker, uint amount) external;
+    function encumber(address taker, uint256 amount) external;
 
     /**
      * @notice Encumbers a specified amount of tokens from a specified owner's balance, with the sender's approval.
@@ -18,7 +18,7 @@ interface IEncumberable {
      * @param taker The address which will have the right to release the encumbered tokens.
      * @param amount The amount of tokens to be encumbered.
      */
-    function encumberFrom(address owner, address taker, uint amount) external;
+    function encumberFrom(address owner, address taker, uint256 amount) external;
 
     /**
      * @notice Releases a specified amount of encumbered tokens back to their owner.
@@ -26,7 +26,7 @@ interface IEncumberable {
      * @param owner The address of the token owner whose encumbered tokens are to be released.
      * @param amount The amount of encumbered tokens to be released.
      */
-    function release(address owner, uint amount) external;
+    function release(address owner, uint256 amount) external;
 
     /**
      * @notice Returns the available balance of tokens for a specified address, excluding encumbered tokens.
@@ -57,114 +57,114 @@ interface IEncumberable {
 interface ISlashable is IEncumberable {
     /**
      * @notice Slashes a specified amount from the encumbered balance of an owner, reducing both their total and encumbered balance.
-     * @dev This function is typically called in scenarios where an encumbered asset needs to be penalized or confiscated due to a breach of contract or similar.
-     *      It is crucial in decentralized finance applications, such as the BondedOracle, where economic incentives and penalties are enforced to ensure compliance and correctness.
-     *      The function requires that the caller (msg.sender) must have sufficient encumbrance rights over the owner's assets to perform the slash.
-     * @param owner The address of the owner whose encumbered assets are being slashed.
-     * @param amount The amount of the assets to slash from the owner's encumbered balance.
      * @dev The caller must have sufficient encumbrance rights over the owner's assets.
      * @dev The owner must have at least `amount` of encumbered assets to be slashed.
+     * @param owner The address of the owner whose encumbered assets are being slashed.
+     * @param amount The amount of the assets to slash from the owner's encumbered balance.
      */
     function slash(address owner, uint256 amount) external;
 }
 
 contract EncumberedToken is ISlashable {
     mapping(address owner => mapping(address spender => uint256 amount)) public allowance;
-    mapping(address => uint256) public balanceOf;
+    mapping(address owner => uint256 amount) public balanceOf;
 
     /// The main difference between encumbrances and allowances is that the owner can't transfer
     /// encumbered funds until the spender releases them back to the owner, but the owner still
     /// retains beneficial ownership of the assets.  While an allowance allows a spender to move
     /// the funds, but the spender doesn't have an entitlement to the funds or a duty to release
-    /// the allowance back to the owner
+    /// the allowance back to the owner. ie, The portion of the users balance that are encumbered
+    /// to various entities are effectively nontransferable in contract to allowances which remain
+    /// transferable by the owner
+
     /// @inheritdoc IEncumberable
     mapping(address owner => mapping(address spender => uint256 amount)) public encumbrances;
     /// @inheritdoc IEncumberable
-    mapping(address => uint256) public encumberedBalanceOf;
+    mapping(address owner => uint256 amount) public encumberedBalanceOf;
 
     /// @notice Encumberable will most likely be applied to a token which already has an approval process
     /// In order to support encumberFrom it needs to be applied with a concept of approval and allowances
-    /// @param spender the account that can move funds from an owner
-    /// @param amount the amount of funds that the spender can move on behalf of the owner
-    function approve(address spender, uint256 amount) external {
-        allowance[msg.sender][spender] = amount;
+    /// @param _spender the account that can move funds from an owner
+    /// @param _amount the amount of funds that the spender can move on behalf of the owner
+    function approve(address _spender, uint256 _amount) external {
+        allowance[msg.sender][_spender] = _amount;
     }
 
     /// @inheritdoc IEncumberable
-    function encumber(address taker, uint amount) external {
-        _encumber(msg.sender, taker, amount);
+    function encumber(address _taker, uint256 _amount) external {
+        _encumber(msg.sender, _taker, _amount);
     }
 
     /// @inheritdoc IEncumberable
-    function encumberFrom(address owner, address taker, uint amount) external {
-        require(allowance[owner][msg.sender] >= amount);
-        _encumber(owner, taker, amount);
+    function encumberFrom(address _owner, address _taker, uint256 _amount) external {
+        require(allowance[_owner][msg.sender] >= _amount);
+        _encumber(_owner, _taker, _amount);
     }
 
     /// @inheritdoc IEncumberable
-    function release(address owner, uint amount) external {
-        _release(owner, msg.sender, amount);
+    function release(address _owner, uint256 _amount) external {
+        _release(_owner, msg.sender, _amount);
     }
 
     /// @inheritdoc IEncumberable
-    function availableBalanceOf(address a) public view returns (uint256) {
-        return (balanceOf[a] - encumberedBalanceOf[a]);
+    function availableBalanceOf(address _owner) public view returns (uint256) {
+        return (balanceOf[_owner] - encumberedBalanceOf[_owner]);
     }
 
     /// @inheritdoc ISlashable
-    function slash(address owner, uint256 amount) external {
-        _spendEncumbrance(owner, msg.sender, amount);
-        balanceOf[owner] -= amount;
+    function slash(address _owner, uint256 _amount) external {
+        _spendEncumbrance(_owner, msg.sender, _amount);
+        balanceOf[_owner] -= _amount;
     }
 
     /**
      * @dev Encumbers a specified amount of the owner's tokens for the taker.
      * This function increases the encumbrance and the encumbered balance for the taker.
      * It requires that the owner has enough available balance to encumber.
-     * @param owner Address of the token owner whose tokens are being encumbered.
-     * @param taker Address of the entity for whom the tokens are encumbered.
-     * @param amount The amount of tokens to be encumbered.
+     * @param _owner Address of the token owner whose tokens are being encumbered.
+     * @param _taker Address of the entity for whom the tokens are encumbered.
+     * @param _amount The amount of tokens to be encumbered.
      */
-    function _encumber(address owner, address taker, uint amount) internal {
-        require(availableBalanceOf(owner) >= amount, "insufficient balance");
-        encumbrances[owner][taker] += amount;
-        encumberedBalanceOf[owner] += amount;
+    function _encumber(address _owner, address _taker, uint256 _amount) internal {
+        require(availableBalanceOf(_owner) >= _amount, "insufficient balance");
+        encumbrances[_owner][_taker] += _amount;
+        encumberedBalanceOf[_owner] += _amount;
     }
 
     /**
      * @dev Releases a specified amount of encumbered tokens of the owner held for the taker.
      * If the requested amount to release is greater than the currently encumbered amount, it releases only the available encumbered amount.
      * This function decreases the encumbrance and the encumbered balance for the taker.
-     * @param owner Address of the token owner whose tokens are being released.
-     * @param taker Address of the entity for whom the tokens are released.
-     * @param amount The amount of tokens to be released.
+     * @param _owner Address of the token owner whose tokens are being released.
+     * @param _taker Address of the entity for whom the tokens are released.
+     * @param _amount The amount of tokens to be released.
      */
-    function _release(address owner, address taker, uint amount) internal {
-        if (encumbrances[owner][taker] < amount) {
-            amount = encumbrances[owner][taker];
+    function _release(address _owner, address _taker, uint256 _amount) internal {
+        if (encumbrances[_owner][_taker] < _amount) {
+            _amount = encumbrances[_owner][_taker];
         }
-        encumbrances[owner][taker] -= amount;
-        encumberedBalanceOf[owner] -= amount;
+        encumbrances[_owner][_taker] -= _amount;
+        encumberedBalanceOf[_owner] -= _amount;
     }
 
     /**
      * @dev Spends a specified amount of encumbered tokens of the owner held for the taker.
      * This function decreases the encumbrance and the encumbered balance for the taker.
      * It requires that the taker has enough encumbered tokens to spend.
-     * @param owner Address of the token owner whose encumbered tokens are being spent.
-     * @param taker Address of the entity that is spending the encumbered tokens.
-     * @param amount The amount of encumbered tokens to be spent.
+     * @param _owner Address of the token owner whose encumbered tokens are being spent.
+     * @param _taker Address of the entity that is spending the encumbered tokens.
+     * @param _amount The amount of encumbered tokens to be spent.
      */
-    function _spendEncumbrance(address owner, address taker, uint256 amount) internal {
-        uint256 currentEncumbrance = encumbrances[owner][taker];
-        require(currentEncumbrance >= amount, "insufficient encumbrance");
-        uint newEncumbrance = currentEncumbrance - amount;
-        encumbrances[owner][taker] = newEncumbrance;
-        encumberedBalanceOf[owner] -= amount;
+    function _spendEncumbrance(address _owner, address _taker, uint256 _amount) internal {
+        uint256 currentEncumbrance = encumbrances[_owner][_taker];
+        require(currentEncumbrance >= _amount, "insufficient encumbrance");
+        uint256 newEncumbrance = currentEncumbrance - _amount;
+        encumbrances[_owner][_taker] = newEncumbrance;
+        encumberedBalanceOf[_owner] -= _amount;
     }
 
     /// For testing
-    function mint(address owner, uint256 amount) external {
-        balanceOf[owner] += amount;
+    function mint(address _owner, uint256 _amount) external {
+        balanceOf[_owner] += _amount;
     }
 }

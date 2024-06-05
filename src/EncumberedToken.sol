8828dda84ae428282d3da08a8530d21fec0394c9
steven nevins <steven.nevins.js@gmail.com>
2024-06-05 07:57:32 -0400

    add some comments

--- src/EncumberedToken.sol
+++ src/EncumberedToken.sol
@@ -3,12 +3,20 @@
 pragma solidity ^0.8.0;
 
 contract Encumberable {
-    // Owner -> Taker -> Amount that can be taken
-    mapping(address => mapping(address => uint)) public encumbrances;
-    mapping(address => mapping(address => uint)) public allowance;
-    mapping(address => uint256) public balanceOf;
+    /// The main difference between encumbrances and allowances is that the owner can't transfer
+    /// encumbered funds until the spender releases them back to the owner, but the owner still
+    /// retains beneficial ownership of the assets.  While an allowance allows a spender to move
+    /// the funds, but the spender doesn't have an entitlement to the funds or a duty to release
+    /// the allowance back to the owner
+    mapping(address owner => mapping(address spender => uint256 amount)) public encumbrances;
+    mapping(address owner => mapping(address spender => uint256 amount)) public allowance;
 
-    mapping(address => uint) public encumberedBalanceOf;
+    mapping(address => uint256) public balanceOf;
+    mapping(address => uint256) public encumberedBalanceOf;
+
+    function approve(address spender, uint256 amount) external {
+        allowance[msg.sender][spender] = amount;
+    }
 
     function encumber(address taker, uint amount) external {
         _encumber(msg.sender, taker, amount);
@@ -23,30 +31,22 @@ contract Encumberable {
         _release(owner, msg.sender, amount);
     }
 
-    function availableBalanceOf(address a) public view returns (uint) {
-        return (balanceOf[a] - encumberedBalanceOf[a]);
-    }
-
-    function approve(address spender, uint256 amount) external {
-        allowance[msg.sender][spender] = amount;
-    }
-
     function slash(address owner, uint256 amount) external {
         _spendEncumbrance(owner, msg.sender, amount);
         balanceOf[owner] -= amount;
     }
 
-    function mint(address owner, uint256 amount) external {
-        balanceOf[owner] += amount;
+    function availableBalanceOf(address a) public view returns (uint256) {
+        return (balanceOf[a] - encumberedBalanceOf[a]);
     }
 
-    function _encumber(address owner, address taker, uint amount) private {
+    function _encumber(address owner, address taker, uint amount) internal {
         require(availableBalanceOf(owner) >= amount, "insufficient balance");
         encumbrances[owner][taker] += amount;
         encumberedBalanceOf[owner] += amount;
     }
 
-    function _release(address owner, address taker, uint amount) private {
+    function _release(address owner, address taker, uint amount) internal {
         if (encumbrances[owner][taker] < amount) {
             amount = encumbrances[owner][taker];
         }
@@ -54,11 +54,16 @@ contract Encumberable {
         encumberedBalanceOf[owner] -= amount;
     }
 
-    function _spendEncumbrance(address owner, address taker, uint256 amount) internal virtual {
+    function _spendEncumbrance(address owner, address taker, uint256 amount) internal {
         uint256 currentEncumbrance = encumbrances[owner][taker];
         require(currentEncumbrance >= amount, "insufficient encumbrance");
         uint newEncumbrance = currentEncumbrance - amount;
         encumbrances[owner][taker] = newEncumbrance;
         encumberedBalanceOf[owner] -= amount;
+    }
+
+    /// For testing
+    function mint(address owner, uint256 amount) external {
+        balanceOf[owner] += amount;
     }
 }
